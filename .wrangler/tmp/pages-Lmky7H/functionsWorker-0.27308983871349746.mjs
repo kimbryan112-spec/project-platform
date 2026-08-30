c64084c4216440765720c279ae8177ead93868f2
KBHFILMS <kimbryan112@gmail.com>
2026-08-30 21:50:36 +0800

    Updated

--- .wrangler/tmp/pages-Lmky7H/functionsWorker-0.27308983871349746.mjs
+++ .wrangler/tmp/pages-Lmky7H/functionsWorker-0.27308983871349746.mjs
@@ -98,7 +98,7 @@ async function onRequestPost(context) {
                     id,
                     fullname,
                     email,
-                    password,
+                    password_hash,
                     role,
                     active
                 FROM users
@@ -133,7 +133,7 @@ async function onRequestPost(context) {
         }
       );
     }
-    if (user.password !== password) {
+    if (user.password_hash !== password) {
       return new Response(
         JSON.stringify({
           success: false,
@@ -1221,7 +1221,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// ../.wrangler/tmp/bundle-J227n7/middleware-insertion-facade.js
+// ../.wrangler/tmp/bundle-8REv7J/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -1253,7 +1253,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// ../.wrangler/tmp/bundle-J227n7/middleware-loader.entry.ts
+// ../.wrangler/tmp/bundle-8REv7J/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
